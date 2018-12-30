abf5896ac912ff1c644fd79af631a44a18937207
Bhanu Pratap Singh <31345924+bpss2010@users.noreply.github.com>
2018-12-30 13:24:49 +0530

    Final working commit

--- index.js
+++ index.js
@@ -32,9 +32,11 @@ connection.connect(function(err) {
 
 
 app.get('/',function(req,res){
+  let msgType = req.query.msgType;
+  let msg     = req.query.msg;
   var query = connection.query('select * from users', function (error, results, fields) {
   if (error) throw error;
-    res.render('index',{resultSet:results});
+    res.render('index',{resultSet:results,msgType:msgType,msg:msg});
   });
 });
 app.get('/addUser',function(req,res){
@@ -44,18 +46,43 @@ app.post('/doSaveData',function(req,res){
   var userData  = {name:req.body.name, email: req.body.email,phone_no:req.body.phone_no,address:req.body.address};
   var query = connection.query('INSERT INTO users SET ?', userData, function (error, results, fields) {
   if (error) throw error;
-    res.redirect('/');
+    res.redirect('/?msgType=success&msg=User Added successfully');
   });
 });
 
 app.get('/editUser/:id',function(req,res){
   let userId = req.params.id;
-  'SELECT * FROM `books` WHERE `author` = "David"'
-  var query = connection.query('SELECT * FROM `users` WHERE `id` = "+userId+"', function (error, results, fields) {
-  if (error) throw error;
-    res.render('edit',{userData:results});
+  var query = connection.query('SELECT * FROM `users` WHERE `id` = '+userId+'', function (error, results, fields) {
+  if (error) {
+      res.redirect('/?msgType=error&msg='+error);
+  }else{
+    res.render('edit',{userData:results});  
+  }
   });
-})
+});
+
+app.post('/doUpdateData/:id',function(req,res){
+    let userId = req.params.id;
+   var query =  connection.query('UPDATE users SET name = ?, email = ?, phone_no = ? , address = ? WHERE id = ?', [req.body.name, req.body.email, req.body.phone_no, req.body.address, userId], function (error, results, fields) {
+      if (error) {
+          res.redirect('/?msgType=error&msg='+error);
+      }else{
+          res.redirect('/?msgType=success&msg=User Updated successfully');
+      }
+      
+    });
+});
+
+app.get('/deleteUser/:id',function(req,res){
+    let userId = req.params.id;
+   var query =  connection.query('DELETE FROM users WHERE id = '+userId+'', function (error, results, fields) {
+      if (error){ 
+         res.redirect('/?msgType=error&msg='+error);
+    }else{
+         res.redirect('/?msgType=success&msg=User Deleted successfully');           
+    }
+    })
+});
 
 app.listen(port,function(){
   console.log('Server runing in port => '+port);
